f4e21772c65cee292d034df203ce2809339e7a01
Waleeed-sheikh <waleedshaikha779@gmail.com>
2024-12-21 20:16:24 +0500

    PROBLEM 342

---
+++ javascript/PowerOf4.js
@@ -0,0 +1,31 @@
+// Easy
+// Topics
+// Companies
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+// Example 1:
+
+// Input: n = 16
+// Output: true
+// Example 2:
+
+// Input: n = 5
+// Output: false
+// Example 3:
+
+// Input: n = 1
+// Output: true
+
+var isPowerOfFour = function(n) {
+    number=1
+    for(number=1;number<n;number=number*4){
+        
+    }
+    if(number==n){
+        return true;
+    }
+    else{
+        return false}
+};
